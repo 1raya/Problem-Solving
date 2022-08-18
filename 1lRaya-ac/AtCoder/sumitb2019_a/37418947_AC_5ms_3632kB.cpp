bd91960aa3553878157bf09cf7e4fd969cd22050
Raya <78345034+1raya@users.noreply.github.com>
2022-08-18 22:36:23 +0300

    Add files via upload

---
+++ 1lRaya-ac/AtCoder/sumitb2019_a/37418947_AC_5ms_3632kB.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m1,m2,d1,d2;
+    cin>>m1>>d1>>m2>>d2;
+    
+    if (m1==m2){
+        cout<<"0";
+    }
+    else{
+        cout<<"1";
+    }
+    return 0;
+}
