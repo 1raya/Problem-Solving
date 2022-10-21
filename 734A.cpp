bbeebc5d269ac7235b9093653e6d193765d545e0
Raya <78345034+1raya@users.noreply.github.com>
2022-10-21 07:20:54 +0300

    Anton and Danik

---
+++ 734A.cpp
@@ -0,0 +1,43 @@
+#define Fast ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define ll long long
+#include <iostream>
+#include <stack>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <map>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+ 
+    int n;
+    cin>>n;
+
+    string s;
+    cin>>s;
+    int cntD=0;
+    int cntA=0;
+    
+ 
+    for (int i=0; i<n; ++i) {
+        if(s[i]=='A'){
+            cntA++;
+        }else if(s[i]=='D'){
+            cntD++;
+        }
+    }
+    
+    if (cntD > cntA) {
+        cout<<"Danik"<<"\n";
+    }else if( cntA > cntD){
+        cout<<"Anton"<<"\n";
+    }else if (cntD == cntA){
+        cout<<"Friendship"<<"\n";
+    }
+   
+
+    return 0;
+}
