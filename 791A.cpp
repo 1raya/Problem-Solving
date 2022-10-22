ae42726514372c659fcd265efcba923d5419c10a
Raya <78345034+1raya@users.noreply.github.com>
2022-10-22 17:07:04 +0300

    Bear and Big Brother

---
+++ 791A.cpp
@@ -0,0 +1,29 @@
+#define Fast ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define ll long long
+#include <iostream>
+#include <stack>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <map>
+#include <cmath>
+
+using namespace std;
+ 
+int main(){
+ Fast
+    int a,b,cnt=0;
+    cin>>a>>b;
+    
+    while(a<=b){
+        a*=3;
+        b*=2;
+        cnt++;
+    }
+    cout<<cnt<<"\n";
+   
+
+    return 0;
+}
+ 
